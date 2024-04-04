aa50717ca023d2fbce55fde1cfa546b6cc36276d
raveena <raveenapatil4@gmail.com>
2024-04-04 16:15:08 +0530

    new

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
-    var audio = document.getElementById("my_audio");
-    audio.play();   
+utyre1445-
+'9l7-5j9h7egbuvtygfve\h t0
+=pc cxfy0   
 
     $('.headerset , #home').click(function () {
         var audio = document.getElementById("my_audio");
@@ -16,6 +17,11 @@ $(document).ready(function(){
               }
           });
     });
-  
-   
+    $("#down").click(function(){
+      document.getElementById('downloadbutton').innerHTML = '<a href="https://example.com/audio.mp3" download="audio.mp3">Download</a>'
+    })
+    function setDownloadLink() {
+      document.getElementById('downloadbutton').innerHTML = '<a href="https://example.com/audio.mp3" download="audio.mp3">Download</a>'
+      //You would need an element around the hyperlink with an ID, then you replace 'downloadbutton' with that ID.
+    }
 });
